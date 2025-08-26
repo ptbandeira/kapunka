bbad059825ff6b6f37ba2b60071d1accf0efe264
Flagster <63302966+ptbandeira@users.noreply.github.com>
2025-08-26 10:59:13 +0200

    chore: ignore body hydration mismatch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,12 @@ import '../../styles/globals.css';
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className="antialiased bg-background text-foreground">{children}</body>
+      <body
+        className="antialiased bg-background text-foreground"
+        suppressHydrationWarning
+      >
+        {children}
+      </body>
     </html>
   );
 }
